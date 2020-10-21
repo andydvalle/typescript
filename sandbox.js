215b0650b7d9c1f7f4c33912b6a48d06d03885d4
github username <andydelvalle89@gmail.com>
2020-10-20 17:50:27 -0700

    learn explicit types

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,32 @@
-//array
-var names = ["luigi", "mario", "yoshi"];
-names.push("toad");
-// names.push(3); //=> error
-// names[0] = 3; //=> error
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('shaun'); //=> error
-// numbers[1] = 'shaun'; //=> error
-var mixed = ["ken", 4, "chun-li", 8, 9];
-mixed.push("ryu"); //=> will work
-mixed.push(10); //=> will work
-mixed[0] = 1; //=> will work
-//objects
-var ninja = {
-    name: "mario",
-    belt: "black",
-    age: 30
-};
-ninja.age = 40; // => will work
-ninja.name = "ryu"; // => will work
-// ninja.age = "30"; // error
-ninja.skills = ["fighting", "sneaking"]; // error can not add additional properties to it
-ninja = {
-    //needs to be the same exact properties
-    name: "yoshi",
-    belt: "orange",
-    age: 40
-};
+// explicit types
+var character; // initializes without a value but infers a string
+var age;
+var isLoggedIn;
+// age = '30'; // error, we've explicitly given age a number type
+age = 30;
+// isLoggedIn = 25; // error
+isLoggedIn = true;
+// arrays
+var ninjas = []; // initialize with an empty array
+// ninjas = [10,23]; // error
+// ninjas = ["yoshi", "mario"];
+// ninjas.push("shaun"); // cannot push something into this array if it wasn't declared as an array
+// union types
+var mixed = []; // use a union parameter
+mixed.push("hello");
+mixed.push(20);
+// mixed.push(false); //error
+console.log(mixed);
+var uid; //don't have to use parenthesis if not in front of array
+uid = "123";
+uid = 123;
+// uid = false; // error
+// objects
+var ninjaOne;
+ninjaOne = { name: "yoshi", age: 30 };
+ninjaOne = "hello"; // error
+ninjaOne = []; // will work because an array is an object
+// declares the type of object
+var ninjaTwo;
+// ninjaTwo = {} // errors, needs all the properties from above with values
+ninjaTwo = { name: "mario", age: 20, beltColour: "black" };
--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,46 @@
-//array
-let names = ["luigi", "mario", "yoshi"];
+// explicit types
+let character: string; // initializes without a value but infers a string
+let age: number;
+let isLoggedIn: boolean;
 
-names.push("toad");
-// names.push(3); //=> error
-// names[0] = 3; //=> error
+// age = '30'; // error, we've explicitly given age a number type
+age = 30;
 
-let numbers = [10, 20, 30, 40];
+// isLoggedIn = 25; // error
+isLoggedIn = true;
 
-numbers.push(25);
-// numbers.push('shaun'); //=> error
-// numbers[1] = 'shaun'; //=> error
+// arrays
+let ninjas: string[] = []; // initialize with an empty array
 
-let mixed = ["ken", 4, "chun-li", 8, 9];
+// ninjas = [10,23]; // error
+// ninjas = ["yoshi", "mario"];
 
-mixed.push("ryu"); //=> will work
-mixed.push(10); //=> will work
-mixed[0] = 1; //=> will work
+// ninjas.push("shaun"); // cannot push something into this array if it wasn't declared as an array
 
-//objects
-let ninja = {
-  name: "mario",
-  belt: "black",
-  age: 30,
+// union types
+let mixed: (string | number)[] = []; // use a union parameter
+mixed.push("hello");
+mixed.push(20);
+// mixed.push(false); //error
+console.log(mixed);
+
+let uid: string | number; //don't have to use parenthesis if not in front of array
+uid = "123";
+uid = 123;
+// uid = false; // error
+
+// objects
+let ninjaOne: object;
+ninjaOne = { name: "yoshi", age: 30 };
+ninjaOne = "hello"; // error
+ninjaOne = []; // will work because an array is an object
+
+// declares the type of object
+let ninjaTwo: {
+  name: string;
+  age: number;
+  beltColour: string;
 };
 
-ninja.age = 40; // => will work
-ninja.name = "ryu"; // => will work
-// ninja.age = "30"; // error
-ninja.skills = ["fighting", "sneaking"]; // error can not add additional properties to it
-
-ninja = {
-  //needs to be the same exact properties
-  name: "yoshi",
-  belt: "orange",
-  age: 40,
-};
+// ninjaTwo = {} // errors, needs all the properties from above with values
+ninjaTwo = { name: "mario", age: 20, beltColour: "black" };
