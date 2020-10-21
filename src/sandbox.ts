3232d36d67d0cfd6f96631dd4608d8e33dd96956
github username <andydelvalle89@gmail.com>
2020-10-21 16:41:33 -0700

    learn function signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,15 +1,32 @@
-// make type aliases
-type StringOrNum = string | number;
-type objWithName = { name: string; uid: StringOrNum };
+// let greet: Function;
 
-const logDetails = (uid: StringOrNum, item: string) => {
-  console.log(`${item} has a uid of ${uid}`);
+// example 1
+let greet: (a: string, b: string) => void;
+//a and b variables don't matter it just says we have two parameters that have to be strings;
+//ts infers it's returning void without having to explicitly write void
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 };
 
-const greet = (user: objWithName) => {
-  console.log(`${user.name} says hello`);
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+//takes three params and returns a number
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === "add") {
+    // if the condition isn't met and doesn't have an else would return undefined which is not a number
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
 };
 
-const greetAgain = (user: objWithName) => {
-  console.log(`${user.name} says hello`);
+// example 3
+let logDetails: (obj: { name: string; age: number }) => void;
+
+//type alias person
+type person = { name: string; age: number };
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
 };
