cd5ac4262fe962603d9ba78a0c3cfbe431e904fc
github username <andydelvalle89@gmail.com>
2020-10-22 17:14:31 -0700

    learn public, private, and read only

--- public/app.js
+++ public/app.js
@@ -1,10 +1,20 @@
 "use strict";
 //classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string; // only allows to read outside and inside the class, can not change the value
+    // private details: string; // makes this property private only allow read and change inside of the class only
+    // amount: number; // default. this is the same as explicitly placing 'public' in front of it. can read and change inside and outside the class.
+    // constructor(c: string, d: string, a: number) {
+    //   this.client = c;
+    //   this.details = d;
+    //   this.amount = a;
+    // }
+    //shorthand of the ^ above commented code
+    function Invoice(client, // you have to put the modifier in front of the variable name
+    details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
         return this.client + " owes $" + this.amount + " for " + this.details;
@@ -14,14 +24,12 @@ var Invoice = /** @class */ (function () {
 var invOne = new Invoice("mario", "work on the mario website", 100);
 var invTwo = new Invoice("luigi", "work on the luigi website", 150);
 var invoices = []; // only adds instances from the Invoice class
-// invoices.push("hello") // error
 invoices.push(invOne);
 invoices.push(invTwo);
-console.log(invOne, invTwo);
-console.log(invoices);
-// can access the different properties of these public Invoice Objects
-invOne.client = "yoshi";
-invTwo.amount = 200;
+invoices.forEach(function (inv) {
+    // console.log(inv.client, inv.details, inv.amount, inv.format()); // details are now private
+    console.log(inv.client, inv.amount, inv.format());
+});
 var form = document.querySelector(".new-item-form");
 // inputs
 var type = document.querySelector("#type");
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 //classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  // readonly client: string; // only allows to read outside and inside the class, can not change the value
+  // private details: string; // makes this property private only allow read and change inside of the class only
+  // amount: number; // default. this is the same as explicitly placing 'public' in front of it. can read and change inside and outside the class.
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  // constructor(c: string, d: string, a: number) {
+  //   this.client = c;
+  //   this.details = d;
+  //   this.amount = a;
+  // }
+
+  //shorthand of the ^ above commented code
+  constructor(
+    readonly client: string, // you have to put the modifier in front of the variable name
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
     return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -19,15 +26,13 @@ const invOne = new Invoice("mario", "work on the mario website", 100);
 const invTwo = new Invoice("luigi", "work on the luigi website", 150);
 
 let invoices: Invoice[] = []; // only adds instances from the Invoice class
-// invoices.push("hello") // error
 invoices.push(invOne);
 invoices.push(invTwo);
-console.log(invOne, invTwo);
-console.log(invoices);
 
-// can access the different properties of these public Invoice Objects
-invOne.client = "yoshi";
-invTwo.amount = 200;
+invoices.forEach((inv) => {
+  // console.log(inv.client, inv.details, inv.amount, inv.format()); // details are now private
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
