ec438c86bbf5d0d5ceb02daa688755944203b80e
github username <andydelvalle89@gmail.com>
2020-10-22 17:45:05 -0700

    learn modules and restructure files

--- public/app.js
+++ public/app.js
@@ -1,42 +1,22 @@
-"use strict";
-//classes
-var Invoice = /** @class */ (function () {
-    // readonly client: string; // only allows to read outside and inside the class, can not change the value
-    // private details: string; // makes this property private only allow read and change inside of the class only
-    // amount: number; // default. this is the same as explicitly placing 'public' in front of it. can read and change inside and outside the class.
-    // constructor(c: string, d: string, a: number) {
-    //   this.client = c;
-    //   this.details = d;
-    //   this.amount = a;
-    // }
-    //shorthand of the ^ above commented code
-    function Invoice(client, // you have to put the modifier in front of the variable name
-    details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes $" + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice("mario", "work on the mario website", 100);
-var invTwo = new Invoice("luigi", "work on the luigi website", 150);
-var invoices = []; // only adds instances from the Invoice class
+import { Invoice } from "./classes/Invoice.js";
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
+let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invoices.forEach(function (inv) {
-    // console.log(inv.client, inv.details, inv.amount, inv.format()); // details are now private
-    console.log(inv.client, inv.amount, inv.format());
+invoices.forEach((inv) => {
+    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
 });
-var form = document.querySelector(".new-item-form");
+const form = document.querySelector(".new-item-form");
+console.log(form.children);
 // inputs
-var type = document.querySelector("#type");
-var tofrom = document.querySelector("#tofrom");
-var details = document.querySelector("#details");
-var amount = document.querySelector("#amount");
-form.addEventListener("submit", function (e) {
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,24 @@
-//classes
-class Invoice {
-  // readonly client: string; // only allows to read outside and inside the class, can not change the value
-  // private details: string; // makes this property private only allow read and change inside of the class only
-  // amount: number; // default. this is the same as explicitly placing 'public' in front of it. can read and change inside and outside the class.
+import { Invoice } from "./classes/Invoice.js";
 
-  // constructor(c: string, d: string, a: number) {
-  //   this.client = c;
-  //   this.details = d;
-  //   this.amount = a;
-  // }
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
-  //shorthand of the ^ above commented code
-  constructor(
-    readonly client: string, // you have to put the modifier in front of the variable name
-    private details: string,
-    public amount: number
-  ) {}
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
 
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`;
-  }
-}
-
-const invOne = new Invoice("mario", "work on the mario website", 100);
-const invTwo = new Invoice("luigi", "work on the luigi website", 150);
-
-let invoices: Invoice[] = []; // only adds instances from the Invoice class
+let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
 invoices.forEach((inv) => {
-  // console.log(inv.client, inv.details, inv.amount, inv.format()); // details are now private
-  console.log(inv.client, inv.amount, inv.format());
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
 });
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
+console.log(form.children);
 
 // inputs
-const type = document.querySelector("#type") as HTMLSelectElement;
+const type = document.querySelector("#type") as HTMLInputElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
